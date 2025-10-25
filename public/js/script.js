f2591d788b1b0cc40e620bbfc3209320a6f505b6
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2025-10-25 14:47:12 -0700

    Added errors and checks for those errors with JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,77 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+// Runs when form is submitted
+document.getElementById("contact-form").onsubmit = () => {
+  clearErrors();
+
+  // Grab Elements
+  // Personal Information
+  let fnameInput = document.getElementById("fname").value.trim();
+  let lnameInput = document.getElementById("lname").value.trim();
+  let emailInput = document.getElementById("email").value.trim();
+
+  // How Did We Meet? Information
+  let metBox = document.getElementById("meet");
+  let otherBox = document.getElementById("other");
+  let optionDisplay = document.getElementById("otherDisplay");
+
+  let mailing = document.getElementById("mailing-list");
+  let format = document.getElementById("email-format-row");
+  let html = document.getElementById("html-button");
+  let text = document.getElementById("text-button");
+
+  let isValid = true;
+  // have default style of error be "none"
+  // but "inline" when requirements not met
+  if (!fnameInput) {
+    isValid = false;
+    document.getElementById("fnameErr").style.display = "inline";
+  }
+
+  if (!lnameInput) {
+    isValid = false;
+    document.getElementById("lnameErr").style.display = "inline";
+  }
+
+  // if mailing list is check
+  if (mailing.checked) {
+    // and email doens't include @ and .
+    if (!emailInput.includes("@") || !emailInput.includes(".")) {
+      isValid = false;
+      document.getElementById("emailErr").style.display = "inline";
+    }
+  }
+
+  // How we Met
+  if (metBox.value == "none") {
+    isValid = false;
+    document.getElementById("meetErr").style.display = "inline";
+  }
+
+  if (metBox.value === "other") {
+    isValid = false;
+    otherBox.style.display = "inline";
+    // if otherBox is empty
+    if (!otherBox) {
+      isValid = false;
+    }
+  }
+
+  // ADD A LISTENER OR A WAY TO APPEAR WHEN MAILING IS CHECKED
+  if (mailing.checked) {
+    format.style.display = "inline";
+  }
+
+  // if both html or text format buttons arent checked
+  if (!html.checked && !text.checked) {
+    isValid = false;
+  }
+
+  return isValid;
+}; // End of onsubmit form
+
+// clears Errors funciton
+function clearErrors() {
+  let errors = document.getElementsByClassName("error");
+  for (let error of errors) {
+    error.style.display = "none";
+  }
 }
-
-// PDF DOWNLOAD BUTTON
-const viewPDF = document.querySelector("#pdf-btn");
-viewPDF.addEventListener("click", () => {
-  window.open("downloads/Kevin-Sandoval-Resume.pdf", "_blank");
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  // LinkedIn click
-  const linkedinIcon = document.getElementById("linkedin-icon");
-  linkedinIcon.addEventListener("click", () => {
-    window.open("https://www.linkedin.com/in/kevinrsandoval", "_blank");
-  });
-
-  // GitHub click
-  const githubIcon = document.getElementById("github-icon");
-  githubIcon.addEventListener("click", () => {
-    window.open("https://github.com/KevinSandoval12", "_blank");
-  });
-});
-
-// Form button
